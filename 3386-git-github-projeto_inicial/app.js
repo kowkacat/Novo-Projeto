1f3af87876d926086127f143b10e188a2578e089
kowka <enzo.frederico23@gmail.com>
2023-11-29 19:57:24 -0300

    deixando o jogo mais facil

--- 3386-git-github-projeto_inicial/app.js
+++ 3386-git-github-projeto_inicial/app.js
@@ -1,7 +1,7 @@
 let listaDeNumerosSorteados = [];
 let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
-let tentativas = 1;
+let tentativas = 1;git remote -v
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
